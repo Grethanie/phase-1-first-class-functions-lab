1a1dc2e8ed877696b82a944937367344669cd99f
Daniel Young <Danyoung91@gmail.com>
2022-11-02 22:29:25 -0400

    commit tree e9f0101b3b909a8366549d98a529ef6c21234cf2

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+  return [array[0], array[1]];
+};
+
+const returnLastTwoDrivers = function (array) {
+  return [array[array.length - 2], array[array.length - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(n) {
+  return function (fare) {
+    return n * fare;
+  };
+}
+
+const fareDoubler = function (fare) {
+  return createFareMultiplier(2)(fare);
+};
+
+const fareTripler = function (fare) {
+  return createFareMultiplier(3)(fare);
+};
+
+function selectDifferentDrivers(arrayOfDrivers, function1) {
+  return function1(arrayOfDrivers);
+}
